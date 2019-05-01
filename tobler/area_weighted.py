8bc446586869f34c1303537ce30cb392e5125fcd
renanxcortes <renanxcortes@gmail.com>
2019-04-30 17:45:36 -0700

    add force_crs_match argument on area_tables_nlcd

--- tobler/area_weighted.py
+++ tobler/area_weighted.py
@@ -184,21 +184,25 @@ def _nan_check(df, column):
         print('nan values in variable: {var}, replacing with 0.0'.format(var=column))
     return values
 
-def area_tables_nlcd(source_df, target_df, raster, codes = [21, 22, 23, 24]):
+def area_tables_nlcd(source_df, target_df, raster, codes = [21, 22, 23, 24], force_crs_match = True):
     """
     Construct area allocation and source-target correspondence tables according to National Land Cover Data (NLCD) 'populated' areas
     Parameters
     ----------
 
-    source_df : geopandas GeoDataFrame with geometry column of polygon type
+    source_df       : geopandas GeoDataFrame with geometry column of polygon type
 
-    target_df : geopandas GeoDataFrame with geometry column of polygon type
+    target_df       : geopandas GeoDataFrame with geometry column of polygon type
     
-    raster    : the associated NLCD raster (from rasterio.open)
+    raster          : the associated NLCD raster (from rasterio.open)
     
-    codes     : an integer list of codes values that should be considered as 'populated' from the National Land Cover Database (NLCD).
-                The description of each code can be found here: https://www.mrlc.gov/sites/default/files/metadata/landcover.html
-                The default is 21 (Developed, Open Space), 22 (Developed, Low Intensity), 23 (Developed, Medium Intensity) and 24 (Developed, High Intensity).
+    codes           : an integer list of codes values that should be considered as 'populated' from the National Land Cover Database (NLCD).
+                      The description of each code can be found here: https://www.mrlc.gov/sites/default/files/metadata/landcover.html
+                      The default is 21 (Developed, Open Space), 22 (Developed, Low Intensity), 23 (Developed, Medium Intensity) and 24 (Developed, High Intensity).
+
+    force_crs_match : bool. Default is True.
+                      Wheter the Coordinate Reference System (CRS) of the polygon will be reprojected to the CRS of the raster file. 
+                      It is recommended to let this argument as True.
 
 
     Returns
@@ -207,8 +211,6 @@ def area_tables_nlcd(source_df, target_df, raster, codes = [21, 22, 23, 24]):
             two 2-D numpy arrays
             SU: area of intersection of source geometry i with union geometry j
             UT: binary mapping of union geometry j to target geometry t
-
-
 
     Notes
     -----
@@ -241,7 +243,7 @@ def area_tables_nlcd(source_df, target_df, raster, codes = [21, 22, 23, 24]):
     res_union_pre.crs = source_df.crs
     
     # The 'append_profile_in_gdf' function is present in nlcd.py script
-    res_union = append_profile_in_gdf(res_union_pre, raster = raster)
+    res_union = append_profile_in_gdf(res_union_pre, raster = raster, force_crs_match = force_crs_match)
     
     str_codes     = [str(i) for i in codes]
     str_list      = ['Type_' + i for i in str_codes]
