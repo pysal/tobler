ad53760ba6901ab215e4a596bd0d9039074579ae
renanxcortes <renanxcortes@gmail.com>
2019-06-06 09:02:00 -0700

    add useful function subset_gdf_polygons_from_raster

--- tobler/vectorized_raster_interpolation.py
+++ tobler/vectorized_raster_interpolation.py
@@ -112,7 +112,8 @@ def append_profile_in_gdf(geodataframe, raster, force_crs_match = True):
     
     """Function that appends the columns of the profile in a geopandas according to a given raster
     
-    geodataframe    : a GeoDataFrame from geopandas. The variables of the profile will be appended in this data.
+    geodataframe    : a GeoDataFrame from geopandas that has overlay with the raster. The variables of the profile will be appended in this data.
+                      If some polygon do not overlay the raster, consider a preprocessing step using the function subset_gdf_polygons_from_raster.
     
     raster          : the associated NLCD raster (from rasterio.open).
     
@@ -593,3 +594,44 @@ def calculate_interpolated_population_from_correspondence_NLCD_table(geodatafram
     final_geodataframe['interpolated_population'] = pop_final
         
     return final_geodataframe
+
+
+
+def subset_gdf_polygons_from_raster(geodataframe, raster, force_crs_match = True):
+    """Function that returns only the polygons that actually have some intersection with a given raster
+    
+    Parameters
+    ----------
+    
+    geodataframe    : a GeoDataFrame from geopandas
+    
+    raster          : the associated raster (from rasterio.open)
+    
+    force_crs_match : bool. Default is True.
+                      Wheter the Coordinate Reference System (CRS) of the polygon will be reprojected to the CRS of the raster file. 
+                      It is recommended to let this argument as True.
+
+    """
+    
+    _check_presence_of_crs(geodataframe)
+    
+    if force_crs_match:
+        reprojected_gdf = geodataframe.to_crs(crs = raster.crs.data)
+    else:
+        warnings.warn('The geodataframe is not being reprojected. The clipping might be being performing on unmatching polygon to the raster.')
+    
+    # within is a boolean vector: True if the polygon is within the raster, False otherwise
+    within = []
+    for i in list(range(len(reprojected_gdf))):
+        print('Polygon {} checked out of {}'.format(i, len(reprojected_gdf)), end = "\r")
+        coords = getFeatures(reprojected_gdf.iloc[[i]])
+        try:
+            out_img = mask(dataset = raster, shapes = coords, crop = True)[0]
+            within.append(True)
+        except:
+            within.append(False)
+    
+    within_gdf = reprojected_gdf.iloc[within]
+    within_gdf = within_gdf.set_geometry('geometry')
+    
+    return within_gdf
