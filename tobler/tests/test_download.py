07fdb7912b6f5f7d8a3aabb51cfef30b573680ee
eli knaap <ek@knaaptime.com>
2020-04-16 18:13:12 -0700

    update tests

--- tobler/tests/test_download.py
+++
@@ -1,5 +0,0 @@
-from tobler.data import store_rasters
-import os
-
-def test_raster_download():
-    store_rasters(os.getcwd())
--- tobler/tests/test_pysal_integration.py
+++ tobler/tests/test_pysal_integration.py
@@ -1,8 +1,6 @@
 """lightweight test for pysal metapckage that functions import."""
 
 def test_imports():
-    import quilt3
     from tobler.dasymetric import masked_area_interpolate
     from tobler.area_weighted import area_interpolate
-    from tobler.data import store_rasters
     from tobler.model import glm, glm_pixel_adjusted
