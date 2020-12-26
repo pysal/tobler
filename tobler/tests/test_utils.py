6e8063e56a5b3573966cbe3f8d94f4aed1e57d98
eli knaap <ek@knaaptime.com>
2020-12-26 15:45:05 -0800

    add h3 to test reqs

--- tobler/tests/test_utils.py
+++ tobler/tests/test_utils.py
@@ -16,3 +16,11 @@ def test_h3fy_nogeoms():
     sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
     sac_hex = h3fy(sac1, return_geoms=False)
     assert len(sac_hex) == 364
+
+def test_h3fy_diff_crs():
+    sac1 = load_example("Sacramento1")
+    sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
+    sac1 = sac1.to_crs(32710)
+    sac_hex = h3fy(sac1, return_geoms=False)
+    assert len(sac_hex) == 364
+    assert sac_hex.crs.to_string() == 'EPSG:32710'
