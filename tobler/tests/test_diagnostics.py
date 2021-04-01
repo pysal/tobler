cfcddf10deed3491e37613621d66c8d72fed0160
AnGWar26 <andrew.warfield@protonmail.com>
2021-04-01 13:20:51 -0700

    create variable to test

--- tobler/tests/test_diagnostics.py
+++ tobler/tests/test_diagnostics.py
@@ -8,6 +8,8 @@ from libpysal.examples import load_example
 
 sac1 = load_example("Sacramento1")
 sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
+sac1["pct_poverty"] = sac1.POV_POP / sac1.POV_TOT
+
 
 def test_smaup():
     queen = Queen.from_dataframe(sac1)
