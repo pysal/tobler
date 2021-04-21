5cfce74862cbfdb49d520a7e25f532448f62cc32
AnGWar26 <andrew.warfield@protonmail.com>
2021-04-20 17:45:17 -0700

    improve smaup test - now tests a real interpolation situation pulled straight from the notebooks

--- tobler/tests/test_diagnostics.py
+++ tobler/tests/test_diagnostics.py
@@ -2,16 +2,15 @@
 
 import geopandas
 from tobler.diagnostics import _smaup
-from libpysal.weights import Queen
-from libpysal.examples import load_example
 
-
-sac1 = load_example("Sacramento1")
-sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
-sac1["pct_poverty"] = sac1.POV_POP / sac1.POV_TOT
-
+precincts = geopandas.read_file("https://ndownloader.figshare.com/files/20460549") 
+tracts = geopandas.read_file("https://ndownloader.figshare.com/files/20460645") 
 
 def test_smaup():
-    queen = Queen.from_dataframe(sac1)
-    s = _smaup(1,sac1["pct_poverty"].to_numpy(),queen)
-    assert s.summary
+    s = _smaup(
+        source_df=tracts,
+        target_df=precincts,
+        y=tracts["pct Youth"].to_numpy()
+    )
+    assert type(s.summary) == str
+    assert s.summary.find('H0 is rejected')
