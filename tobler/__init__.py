710c802bade19549ece4a676d77e5b1b7c47e299
eli knaap <ek@knaaptime.com>
2023-05-09 09:58:08 -0700

    tobler not segregation in init

--- tobler/__init__.py
+++ tobler/__init__.py
@@ -9,4 +9,4 @@ from importlib.metadata import PackageNotFoundError, version
 from . import area_weighted, dasymetric, model, pycno, util
 
 with contextlib.suppress(PackageNotFoundError):
-    __version__ = version("segregation")
+    __version__ = version("tobler")
