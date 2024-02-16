b9a7cb535404a40c2c009894da7832a0f8148f0f
eli knaap <ek@knaaptime.com>
2024-02-15 16:43:17 -0800

    reload data on each test

--- tobler/tests/test_utils.py
+++ tobler/tests/test_utils.py
@@ -9,16 +9,17 @@ from numpy.testing import assert_almost_equal
 
 from tobler.util import h3fy
 
-sac1 = load_example("Sacramento1")
-sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
-
 
 def test_h3fy():
+    sac1 = load_example("Sacramento1")
+    sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
     sac_hex = h3fy(sac1, return_geoms=True)
     assert sac_hex.shape == (364, 1)
 
 
 def test_h3fy_nogeoms():
+    sac1 = load_example("Sacramento1")
+    sac1 = geopandas.read_file(sac1.get_path("sacramentot2.shp"))
     sac_hex = h3fy(sac1, return_geoms=False)
     assert len(sac_hex) == 364
 
