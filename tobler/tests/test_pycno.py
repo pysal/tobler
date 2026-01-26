8f30f2f8239257a8169bb18723f1ce7e4482cd39
James Gaboardi <jgaboardi@gmail.com>
2026-01-25 21:16:50 -0500

    missed 1 astropy warning

--- tobler/tests/test_pycno.py
+++ tobler/tests/test_pycno.py
@@ -9,6 +9,16 @@ from numpy.testing import assert_almost_equal
 from tobler.pycno import pycno_interpolate
 
 
+astropy_warning = pytest.warns(
+    AstropyUserWarning,
+    match=(
+        "nan_treatment='interpolate', however, NaN values detected post "
+        "convolution. A contiguous region of NaN values, larger than the kernel "
+        "size, are present in the input array."
+    ),
+)
+
+
 def datasets():
     sac1 = load_example("Sacramento1")
     sac2 = load_example("Sacramento2")
@@ -23,23 +33,17 @@ def datasets():
 
 def test_pycno_interpolate():
     sac1, sac2 = datasets()
-    pyc = pycno_interpolate(
-        source_df=sac1, target_df=sac2, variables=["TOT_POP"], cellsize=500
-    )
+    with astropy_warning:
+        pyc = pycno_interpolate(
+            source_df=sac1, target_df=sac2, variables=["TOT_POP"], cellsize=500
+        )
     assert_almost_equal(pyc.TOT_POP.sum(), 1794618.503, decimal=1)
 
 
 def test_custom_index():
     sac1, sac2 = datasets()
     sac2 = sac2.set_index("ZIP")
-    with pytest.warns(
-        AstropyUserWarning,
-        match=(
-            "nan_treatment='interpolate', however, NaN values detected post "
-            "convolution. A contiguous region of NaN values, larger than the kernel "
-            "size, are present in the input array."
-        ),
-    ):
+    with astropy_warning:
         pyc = pycno_interpolate(
             source_df=sac1, target_df=sac2, variables=["TOT_POP"], cellsize=500
         )
